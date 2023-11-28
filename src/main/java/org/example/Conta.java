683f46a02e0e26baff3986302c287f4e2f37e733
akyamadesu1 <akyama.megumi@gmail.com>
2023-11-28 20:10:10 -0300

    correções

--- src/main/java/org/example/Conta.java
+++ src/main/java/org/example/Conta.java
@@ -15,21 +15,23 @@ public class Conta {
     /**
      * Um cliente comum com saldo atual de -300 reais
      * @param int1
+     * @return saldo atual
      */
     @Given("um cliente comum com saldo atual de {int} reais")
     public void um_cliente_comum_com_saldo_atual_de_reais(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Saldo atual: " + int1 + " reais");
     }
 
     /**
      * Solicitar um saque de 200 reais
      * @param int1
+     * @return saque do valor desejado
      */
     @When("solicitar um saque de {int} reais")
     public void solicitar_um_saque_de_reais(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Valor a sacar: " + int1 + " reais");
     }
 
     /**
@@ -39,18 +41,19 @@ public class Conta {
     @Then("não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente")
     public void não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Saldo insuficiente");
     }
 
 
     /**
      * Um cliente especial com saldo atual de -200 reais
      * @param int1
+     * @return saldo atual
      */
     @Given("um cliente especial com saldo atual de {int} reais")
     public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Saldo: " + int1 + " reais");
     }
 
     /**
@@ -60,18 +63,20 @@ public class Conta {
     @When("for solicitado um saque no valor de {int} reais")
     public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Valor a sacar: " + int1 + " reais");
     }
 
     /**
      * Deve efetuar o saque e atualizar o saldo da conta para -300 reais
+     * @param int1
      * @return mensagem "Saldo da conta -R$300,00"
      */
     @Then("deve efetuar o saque e atualizar o saldo da conta para {int} reais")
     public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Saldo: " + int1 + " reais");
     }
 
-}
 
+
+}
